788a38af7fbf7d09a969a505566f9cee06812a68
Francisco Huertas <francisco@fhuertas.com>
2015-11-09 12:41:44 +0100

    Prepare environment

---
+++ m0/src/test/scala/com/fhuertas/scalacourse/AppTest.scala
@@ -0,0 +1,22 @@
+package com.fhuertas.scalacourse
+
+import org.junit._
+import Assert._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+//import org.specs2.runner.JUnitRunner
+
+
+
+@RunWith(classOf[JUnitRunner])
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+    //    @Test
+    //    def testKO() = assertTrue(false)
+
+}
+
+
